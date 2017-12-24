086da7187d878b053c84b9441fae22d8e7b4399b
pranshulsaini <pranshulsaini17@gmail.com>
2017-12-24 12:04:53 +0100

    withinTrial works now; Added min_jerk

--- Trajectory/min_jerk.m
+++ Trajectory/min_jerk.m
@@ -1,82 +1,5 @@
-% in this code we'll try to expand the single state simulation to
-% trajectories.
-
-% let's start by showing the single state adaptation
-
-
-close all
-
-parad = [zeros(1,50) ones(1,500)  -1.*ones(1,50) zeros(1,100) ones(1,120)];
-% parad = [zeros(1,50) linspace(0,1,50)  -1.*ones(1,14) zeros(1,50) ones(1,120)];
-
-V     = 0.999;
-Alpha = 0.01;
-x     = zeros(1,numel(parad));
-
-% single state
-for i = 2:numel(parad)
-    error = parad(i-1) - x(i-1) ;
-    x(i) = V*x(i-1) + Alpha*error;
-end
-figure; hold on;
-plot(parad, 'k');
-plot(x);
-legend('Paradigm', 'Adaptation', 'Location', 'southwest')
-% title('single state')
-
-%% What we would like to have:
-
-% 1) defined force field field
-% 2) represent hand as a point mass
-% And then having the hand adapt to the force field
-
 %% We'll start by simulating a point mass in 2D and apply a force on it and see the trajectory
-
-% NOTE: everthing is represented in rows -> [x, y]
-
-% define mass ad force
-m  = 0.250;          % [kg] -> average mass of the hand
-f  = [0, 0.01];   % [N]  -> force applied on the hand
-
-
-% force field
-angle = 90;
-
-% simulation params
-dt     = 0.1;    % [s]  -> step size
-period = 40; % [s]
-N      = period/dt; 
-
-r  = [0, 0]; % initial position
-v  = [0, 0]; % initial speed
-
-% here is were the loop should be implemented
-figure; grid; hold on
-for i = 1:N
-    
-    % calculate the acceleration
-    a = f./m;
-    
-    % update velocity
-    v = a.*dt + v;
-    
-    % update position
-    r = r + v.*dt + 0.5.*a.*dt^2; % in principle, we should not have acceleration term here
-    
-    scatter(r(1), r(2)); xlim([-20,20]); ylim([-20,20]);
-    
-    pause(0.01)
-    
-    
-    if mod(i,30)==0
-        f = (rand(1,2)-0.5)./10; %(rand(1,2)-0.5)./10  % 0.07 .* rotateMat(v, angle);
-    end
-    
-end
-
-%% Now we will add a target
-
-%% We'll start by simulating a point mass in 2D and apply a force on it and see the trajectory
+%%%%%%%%%%%%%%%%%%%%%% Minimum Jerk Method %%%%%%%%%%%%%%%%%%%%%%%
 % Two problems left: the task needs to be completed in the given time
 % (modify force accordingly)
 % make it adaptable to different force fields
@@ -108,11 +31,14 @@ f             = [0 0];
 
 % simulation params
 dt     = 0.1;    % [s]  -> step size
-period = 50; % [s]
-N      = period/dt; 
+t_f = 50; % [s]
+N      = t_f/dt; 
 
 r  = startPos; % initial position
 v  = [0, 0]; % initial speed
+
+r_past = 0;
+r1_past = 0;
 
 % here is were the loop should be implemented
 figure; grid; hold on
@@ -131,15 +57,33 @@ for i = 1:N
     f_adapt = -1 .* f_forcefield;
     f_hand  = f_adapt + f_goal; 
     f       = f_hand + f_forcefield;  % This is the net force influencing the motion
-    
-    % calculate the acceleration 
-    %a = f./m;  % can't be done. It's not an external system under observation
-    
-    % update velocity
-    %v = a.*dt + v;  % not correct
-    
+
     % I think we need to map the force directly to velocity
     v = f *10;
+    
+    % Implementation of minimum jerk methof
+    D =  t_f - t;
+    tau = t/D;
+    
+    r1 = (r - r_past)/dt;
+    r2 = (r1 - r1_past)/dt;
+    
+    q = [r r1 r2]; % 2x3
+    A = [0 0 -60/D^3; 1 0 -36/D^2; 0 1 -9/D]; % 3x3
+    B = [0 0 60/D^3]; % 1x3
+    B = repmat(B,2,1); %2x3
+    
+    a0 = r;
+    a1 = D.*r1;
+    a2 = D.*r2/2;
+    a3 = (-(3*D^2)/2)*r - 6*D*r1 + 10*(target_pos - r);
+    a4 = ((3*D^2)/2)*r + 8*D*r1 - 15*(target_pos - r);
+    a5 = (-(D^2)/2)*r - 3*D*r1 + 6*(target_pos - r);
+    
+    
+    q1 = q * A + B.* repmat(r',1,3);  % repmat has to be used. See the paper
+
+        
     
     % update position
     r = r + v.*dt; % + 0.5.*a.*dt^2;
@@ -156,5 +100,8 @@ for i = 1:N
         f_forcefield = 0.07 .* rotateMat(v, angle); %(rand(1,2)-0.5)./10
     end
 
+    r_past = r;
+    r1_past = r1;
+    
     pause(0.001)
 end
